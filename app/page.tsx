b39e4dc34c9dabe49f77b98facd19ff2b4db2295
Brucedevnairobi <bruce14simiyu@gmail.com>
2024-02-02 22:56:41 +0300

    NextJS being a little bitch

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="flex h-5  items-center bg-amber-600 p-8 font-bold text-red-600">
-        Hello World
-      </h1>
+      <h1 className="h1-bold">Hello World</h1>
+      <h1 className="h2-bold">Hello World</h1>
+      <h1 className="h3-bold">Hello World</h1>
     </div>
   );
 }
