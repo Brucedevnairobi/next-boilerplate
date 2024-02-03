bcb9556747127a396522088c378f28461f0cb36c
Brucedevnairobi <bruce14simiyu@gmail.com>
2024-02-03 23:09:53 +0300

    Added auth but working on secret keys

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-
-
-const Home = () => {
+import { UserButton } from "@clerk/nextjs";
+ 
+export default function Home() {
   return (
-    <div className='bg-red-900'>page</div>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
-
-export default Home;
