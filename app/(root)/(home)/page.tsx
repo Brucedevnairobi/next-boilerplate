0af6c778e2656a12d0528c01cb34db2d1c0d0dcf
Brucedevnairobi <bruce14simiyu@gmail.com>
2024-02-04 23:40:59 +0300

    Handles Metadata, fonts, and Icon

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
- 
 export default function Home() {
   return (
-    <div className="h-screen">
-      <UserButton afterSignOutUrl="/"/>
+    <div className="h-screen bg-black text-center text-white">
+      <h1>Hello World</h1>
     </div>
-  )
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,26 @@
 import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import { ClerkProvider } from "@clerk/nextjs";
-const inter = Inter({ subsets: ["latin"] });
+
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: '--font-inter'
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: '--font-space-grotesk'
+});
 
 export const metadata: Metadata = {
   title: "Dev Overflow",
-  description: "A community driven platform for asking and answering programming questions",
+  description:
+    "A community driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers outside the world. Explore topics in web development, mobile app development, algorithms, data structures and more.",
   icons: {
     icon: "/assets/images/site-logo.svg",
   },
@@ -19,17 +32,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider 
-    appearance={{
-      elements: {
-        formButtonPrimary: 'primary-gradient',
-        footerActionLink: 'primary-text-gradient
-        hover:text-primary-500'
-      }
-    }}>
+  
       <html lang="en">
-        <body className={`${inter.variable} ${Space_Grotesk.variable}`}>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
-    </ClerkProvider>
+
   );
 }
